859129c1a4d3083aec16e524ce4ff9416811bda1
João Victor Lomazzi <jvlomazzi@hotmail.com>
2021-06-06 20:45:46 -0300

    Deploy Index & Start Transparencia

--- js/script.js
+++ js/script.js
@@ -1,4 +1,52 @@
-$("#night_mode").change(function(){
-    console.log("Teste");
+$(window).on('scroll', function () {
+    //header fixed animation and control
+    if ($(window).scrollTop() > 100) {
+        $('header').addClass('header-fixed');
+    } else {
+        $('header').removeClass('header-fixed');
+    }
+    //     //back to top button control
+    //     if ($(window).scrollTop() > 300) {
+    //         $('#back-to-top').addClass('back-btn-shown');
+    //     } else {
+    //         $('#back-to-top').removeClass('back-btn-shown');
+    //     }
 });
-console.log("iniciou arquivo");
+
+
+
+$('.carousel').swipe( {
+    swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+
+        if(direction=='left'){
+            $(this).carousel('next');
+        }else if(direction=='right'){
+            $(this).carousel('prev');
+        }
+
+    }
+});
+
+$('.dropdown').hover(function(e){ $(this).children('.dropdown-toggle').dropdown("toggle"); }, function(){ $(this).children('.dropdown-toggle').dropdown("toggle"); });
+
+// $(window).resize(function(){
+//     if($( window ).width() <= 1199)
+//         $(".gov-info").removeClass("col-md-3").addClass("col-md-6");
+//     else if($(".gov-info").hasClass("col-md-6"))
+//         $(".gov-info").removeClass("col-md-6").addClass("col-md-3");
+// });
+
+$(document).on('click','.side-menu-wrap .side-menu-ul .sidenav__item .menu-plus-icon', function () {
+    $(this).closest('.sidenav__item').siblings().removeClass('active').find('.side-sub-menu').slideUp(200);
+    $(this).closest('.sidenav__item').toggleClass('active').find('.side-sub-menu').slideToggle(200);
+    return false;
+});
+/*=========== Mobile Menu Open Control ============*/
+$(document).on('click','.mobile-menu-toggle', function () {
+    $('.side-nav-container').addClass('active');
+});
+
+/*=========== Mobile Menu Close Control ============*/
+$(document).on('click','.side-menu-close', function () {
+    $(".side-nav-container").removeClass('active');
+});
---
+++ transparencia.php
@@ -0,0 +1,218 @@
+<?php include "header.html"; ?>
+
+<style>
+    .section-page-presentation{
+        padding: 50px 0;
+        background-image: linear-gradient(-21deg, #1161AC, rgb(34, 179, 84));
+        color: rgb(255, 255, 255);
+    }
+    .section-page-presentation .col-md-12{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+    .section-page-presentation .col-md-12 div{
+
+    }
+    .container-card-transparencia{
+        margin: 20px auto;
+    }
+    .presentation-title{
+        text-transform: uppercase;
+        font-size: 50px;
+        font-weight: 800;
+    }
+    .presentation-description{
+        font-size: 21px;
+        font-weight: 300;
+    }
+
+    .card-header span::before{
+        content: "";
+        width: 25px;
+        height: 22px;
+        float: left;
+        margin-right: 10px;
+        margin-left: -10px;
+        border-radius: 5px;
+    }
+    .card-1::before{ background: rgb(27, 143, 75); }
+    .card-1, .title-1{ color: rgb(27, 143, 75); }
+    .card-2::before{ background: rgb(17, 97, 172); }
+    .card-2, .title-2{ color: rgb(17, 97, 172); }
+    .card-3::before{ background: rgb(216, 0, 0); }
+    .card-3, .title-3{ color: rgb(216, 0, 0); }
+</style>
+<section class="section-page-presentation">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div>
+                    <p class="presentation-title">Acesso à informação</p>
+                    <p class="presentation-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lobortis rutrum congue. 
+                        Sed tempor magna quis feugiat blandit. Cras sed elementum eros. Aenean eu suscipit purus. 
+                        Ut dictum varius pretium. Ut efficitur lorem ac neque sodales.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section-servicos">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 container-card-transparencia">
+                <div class="card">
+                    <div class="card-header">
+                        <span class="card-1">Titulo</span>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title title-1">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 container-card-transparencia">
+                <div class="card">
+                    <div class="card-header">
+                        <span class="card-2">Titulo</span>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title title-2">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 container-card-transparencia">
+                <div class="card">
+                    <div class="card-header">
+                        <span class="card-3">Titulo</span>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title title-3">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 container-card-transparencia">
+                <div class="card">
+                    <div class="card-header">
+                        <span>Titulo</span>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 container-card-transparencia">
+                <div class="card">
+                    <div class="card-header">
+                        <span>Titulo</span>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 container-card-transparencia">
+                <div class="card">
+                    <div class="card-header">
+                        <span>Titulo</span>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 container-card-transparencia">
+                <div class="card">
+                    <div class="card-header">
+                        <span>Titulo</span>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 container-card-transparencia">
+                <div class="card">
+                    <div class="card-header">
+                        <span>Titulo</span>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 container-card-transparencia">
+                <div class="card">
+                    <div class="card-header">
+                        <span>Titulo</span>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 container-card-transparencia">
+                <div class="card">
+                    <div class="card-header">
+                        <span>Titulo</span>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 container-card-transparencia">
+                <div class="card">
+                    <div class="card-header">
+                    <span>Titulo</span>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 container-card-transparencia">
+                <div class="card">
+                    <div class="card-header">
+                        <span>Titulo</span>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include "footer.html"; ?>
